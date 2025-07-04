9fc1c8463ee42b154585c6499bc0d83ef0805133
E-cmyk-ux <mohamedelsamady84@gmail.com>
2025-07-05 00:20:15 +0300

    category ad create , edit , delete

--- Cinema_task/Areas/Admin/Controllers/CategoryController.cs
+++ Cinema_task/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Cinema_task.Data;
+using Cinema_task.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,12 +16,54 @@ namespace Cinema_task.Areas.Admin.Controllers
             return View(categories);
         }
 
-     
-        public IActionResult Index1()//createpage 
+        [HttpGet]
+        public IActionResult Index1()//create page 
         {   
             return View();
         }
+        [HttpPost]
+        public IActionResult Index1(Categories Categories)//create page 
+        {
 
+            _context.Add(Categories);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+            
+        }
+        [HttpGet]
+        public IActionResult Edit([FromRoute] int id)
+        {
+            var Categories = _context.Categories.Find(id);
+
+            return View(Categories);
+        }
+        //return RedirectToAction(actionMame: "NotFoundPage", controllertumo: "Home"); 
+        
+        [HttpPost]
+        public IActionResult Edit(Categories Categories)
+        {
+            _context.Update(Categories);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var Categories = _context.Categories.Find(id);
+
+            if (Categories is not null)
+            {
+                _context.Remove(Categories);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return NotFound();
+        }
 
     }
 }
